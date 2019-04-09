104bc6dcc68239230ad641428cf81fe95e08efeb
Liveshort <m.kortleven@student.tudelft.nl>
2019-04-09 10:31:15 +0200

    updated flatfield analysis with new data

--- supporting-scripts/flatfield-analysis/ff_analysis.py
+++ supporting-scripts/flatfield-analysis/ff_analysis.py
@@ -17,98 +17,20 @@ if __name__ == "__main__":
     with open("nir.field", 'rb') as f:
         nir = np.load(f)
 
-    # create a list for the calculated reflectances
-    R_nir = []
-    R_red = []
-
-    # calculate for the lower right corner of the picture
-    nir_zeroth = np.mean(nir[950:975, 1175:1200])
-    nir_first = np.mean(nir[1070:1095, 1195:1220])
-    nir_second = np.mean(nir[1070:1095, 1260:1285])
-    nir_black = np.mean(nir[625:650, 1515:1540])
-
-    R_nir.append((nir_first-nir_black)/(nir_zeroth-nir_black))
-    R_nir.append((nir_second-nir_black)/(nir_first-nir_black))
-
-    red_zeroth = np.mean(red[950:975, 1175:1200])
-    red_first = np.mean(red[1070:1095, 1195:1220])
-    red_second = np.mean(red[1070:1095, 1260:1285])
-    red_black = np.mean(red[625:650, 1515:1540])
-
-    R_red.append((red_first-red_black)/(red_zeroth-red_black))
-    R_red.append((red_second-red_black)/(red_first-red_black))
-
-    # calculate for the upper right corner
-    nir_zeroth = np.mean(nir[195:220, 1150:1175])
-    nir_first = np.mean(nir[195:220, 1220:1245])
-    nir_second = np.mean(nir[130:155, 1220:1245])
-    nir_black = np.mean(nir[625:650, 1515:1540])
-
-    R_nir.append((nir_first-nir_black)/(nir_zeroth-nir_black))
-    R_nir.append((nir_second-nir_black)/(nir_first-nir_black))
-
-    red_zeroth = np.mean(red[195:220, 1150:1175])
-    red_first = np.mean(red[195:220, 1220:1245])
-    red_second = np.mean(red[130:155, 1220:1245])
-    red_black = np.mean(red[625:650, 1515:1540])
-
-    R_red.append((red_first-red_black)/(red_zeroth-red_black))
-    R_red.append((red_second-red_black)/(red_first-red_black))
-
-    # calculate for the upper left corner
-    nir_zeroth = np.mean(nir[198:223, 410:435])
-    nir_first = np.mean(nir[198:223, 275:300])
-    nir_second = np.mean(nir[125:150, 275:300])
-    nir_black = np.mean(nir[625:650, 1515:1540])
-
-    R_nir.append((nir_first-nir_black)/(nir_zeroth-nir_black))
-    R_nir.append((nir_second-nir_black)/(nir_first-nir_black))
-
-    red_zeroth = np.mean(red[198:223, 410:435])
-    red_first = np.mean(red[198:223, 275:300])
-    red_second = np.mean(red[125:150, 275:300])
-    red_black = np.mean(red[625:650, 1515:1540])
-
-    R_red.append((red_first-red_black)/(red_zeroth-red_black))
-    R_red.append((red_second-red_black)/(red_first-red_black))
-
-    # calculate for the lower left corner
-    nir_zeroth = np.mean(nir[980:1005, 345:370])
-    nir_first = np.mean(nir[1075:1100, 340:365])
-    nir_second = np.mean(nir[1075:1100, 250:275])
-    nir_black = np.mean(nir[625:650, 1515:1540])
-
-    R_nir.append((nir_first-nir_black)/(nir_zeroth-nir_black))
-    R_nir.append((nir_second-nir_black)/(nir_first-nir_black))
-
-    red_zeroth = np.mean(red[980:1005, 345:370])
-    red_first = np.mean(red[1075:1100, 340:365])
-    red_second = np.mean(red[1075:1100, 250:275])
-    red_black = np.mean(red[625:650, 1515:1540])
-
-    R_red.append((red_first-red_black)/(red_zeroth-red_black))
-    R_red.append((red_second-red_black)/(red_first-red_black))
-
-    R_nir = np.array(R_nir)
-    R_red = np.array(R_red)
-
-    print("red reflectance: {:.2f} \pm {:.2f}".format(np.mean(R_red), np.std(R_red)))
-    print("nir reflectance: {:.2f} \pm {:.2f}".format(np.mean(R_nir), np.std(R_nir)))
-
     plt.figure()
     plt.imshow(red)
     plt.colorbar()
     plt.show(block=False)
 
     plt.figure()
-    plt.plot(get_row(red, 200))
-    plt.plot(get_row(red, 400))
-    plt.plot(get_row(red, 600))
-    plt.plot(get_row(red, 800))
-    plt.plot(get_row(red, 1000))
+    plt.plot(get_row(red, 100))
+    plt.plot(get_row(red, 275))
+    plt.plot(get_row(red, 475))
+    plt.plot(get_row(red, 675))
+    plt.plot(get_row(red, 850))
     plt.xlabel("pixel [-]")
     plt.ylabel("value [-]")
-    plt.legend(["px 200", "px 400", "px 600", "px 800", "px 1000"])
+    plt.legend(["px 100", "px 275", "px 475", "px 675", "px 850"])
     plt.show(block=False)
 
     plt.figure()
@@ -117,12 +39,12 @@ if __name__ == "__main__":
     plt.show(block=False)
 
     plt.figure()
-    plt.plot(get_row(nir, 200))
-    plt.plot(get_row(nir, 400))
-    plt.plot(get_row(nir, 600))
-    plt.plot(get_row(nir, 800))
-    plt.plot(get_row(nir, 1000))
+    plt.plot(get_row(nir, 100))
+    plt.plot(get_row(nir, 275))
+    plt.plot(get_row(nir, 475))
+    plt.plot(get_row(nir, 675))
+    plt.plot(get_row(nir, 850))
     plt.xlabel("pixel [-]")
     plt.ylabel("value [-]")
-    plt.legend(["px 200", "px 400", "px 600", "px 800", "px 1000"])
+    plt.legend(["px 100", "px 275", "px 475", "px 675", "px 850"])
     plt.show()
